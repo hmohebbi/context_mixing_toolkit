4625d711e385fc7a84dc47c5ccf775713f58088e
hmohebbi <hosein.mohebbi75@gmail.com>
2024-03-15 01:30:09 +0100

    fixed div

--- src/modeling_gemma.py
+++ src/modeling_gemma.py
@@ -961,7 +961,8 @@ class GemmaModel(GemmaPreTrainedModel):
                     # normalizing to sum 1 for each row
                     sums = torch.sum(vz_matrix, axis=-1, keepdims=True)
                     mask = torch.all(sums == 0, axis=-1, keepdims=True)
-                    vz_matrix = torch.divide(vz_matrix, sums, out=torch.zeros_like(vz_matrix), where=~mask, rounding_mode='trunc')
+                    vz_matrix[~mask] = torch.div(vz_matrix[~mask], sums[~mask])
+                    vz_matrix[mask] = 0
 
 
             hidden_states = layer_outputs[0]
